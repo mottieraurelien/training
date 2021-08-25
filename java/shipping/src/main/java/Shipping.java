0eccdd339c3967045cf560d87284655affebe992
AurélienMottier <amottier@palo-it.com>
2021-08-25 21:47:20 +0800

    [Job interview] : first exercise on Java to compute the number of packages needed => solved.

---
+++ java/shipping/src/main/java/Shipping.java
@@ -0,0 +1,25 @@
+import static java.lang.Math.min;
+
+public class Shipping {
+
+    private static final int ITEMS_PER_LARGE_PACKAGE = 5;
+    private static final int ITEMS_PER_SMALL_PACKAGE = 1;
+
+    public static int minimalNumberOfPackages(final int items, final int availableLargePackages, final int availableSmallPackages) {
+
+        final int maxNumberOfItems = availableLargePackages * ITEMS_PER_LARGE_PACKAGE + availableSmallPackages * ITEMS_PER_SMALL_PACKAGE;
+
+        if (items > maxNumberOfItems) return -1;
+
+        final int numberOfLargePackagesNeeded = min(items / ITEMS_PER_LARGE_PACKAGE, availableLargePackages);
+        final int remainingItemsToPack = items - numberOfLargePackagesNeeded * ITEMS_PER_LARGE_PACKAGE;
+        final int minimalNumberOfPackagesNeeded = remainingItemsToPack / ITEMS_PER_SMALL_PACKAGE;
+
+        return numberOfLargePackagesNeeded + minimalNumberOfPackagesNeeded;
+    }
+
+    public static void main(final String[] args) {
+        System.out.println(minimalNumberOfPackages(16, 2, 10));
+    }
+
+}
